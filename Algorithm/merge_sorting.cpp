a28fe6c72b66cf8ba544ff0793e64f24302f0f62
ChenZihan <killbugs@yeah.net>
2023-09-14 20:06:14 +0800

    Add merge sorting

--- Algorithm/merge_sorting.cpp
+++ Algorithm/merge_sorting.cpp
@@ -5,4 +5,65 @@
 
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
 #include <iostream>
+
+int arr[10] = {11, 5, 2, 9, 3, 10, 6, 8, 7, 1};
+
+int arr2[10] = {2, 3, 4, 5, 9, 1, 6, 7, 8, 10};
+
+#define ARR_END_FLAG 0xFFFFFFFF
+void merge_inner(int* arr, size_t arr_size)
+{
+  int arr1_size = arr_size / 2;
+  int arr2_size = arr_size - arr1_size;
+  int* arr1 = (int*)malloc((arr1_size + 1) * sizeof(int));
+  int* arr2 = (int*)malloc((arr2_size + 1) * sizeof(int));
+  memcpy(arr1, arr, (arr1_size + 1) * sizeof(int));
+  memcpy(arr2, &arr[arr1_size], (arr1_size + 1) * sizeof(int));
+  arr1[arr1_size] = ARR_END_FLAG;
+  arr2[arr2_size] = ARR_END_FLAG;
+
+  size_t arr1_idx = 0;
+  size_t arr2_idx = 0;
+  for (size_t i = 0; i < arr_size; i++) {
+
+    if (arr1[arr1_idx] == ARR_END_FLAG) {
+      arr[i] = arr2[arr2_idx];
+      arr2_idx++;
+      continue;
+    }
+    else if (arr2[arr2_idx] == ARR_END_FLAG) {
+      arr[i] = arr1[arr1_idx];
+      arr1_idx++;
+      continue;
+    }
+
+    if (arr1[arr1_idx] > arr2[arr2_idx]) {
+      arr[i] = arr2[arr2_idx];
+      arr2_idx++;
+    }
+    else {
+      arr[i] = arr1[arr1_idx];
+      arr1_idx++;
+    }
+  }
+
+  free(arr1);
+  free(arr2);
+}
+
+void merge_sorting(int* arr, size_t arr_size)
+{
+  merge_inner(&arr[arr_size / 2], arr_size);
+  merge_sorting();
+}
+
+int main()
+{
+  int arr_size = 10;
+  merge_inner(arr2, arr_size);
+  for (size_t i = 0; i < arr_size; i++) {
+    printf("%3d ", arr2[i]);
+  }
+}
